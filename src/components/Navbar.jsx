70fc354efca137ece3e844f04b9b703de5189bcc
TEMOSO-MOJAPELO <btemoso7@gmail.com>
2024-09-25 22:51:40 +0200

    href update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -154,7 +154,7 @@ const Navbar = () => {
                     </a>
                 </li>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-800 '> 
-                    <a className='flex justify-between items-center w-full text-grey-300'>
+                    <a className='flex justify-between items-center w-full text-grey-300' href="/">
                         BitBucket <FaBitbucket size={30}/>
                     </a>
                 </li>
